b1e4b44f5ebec3850975856a1403c3f8a8aa736f
Ardekani <babak.ardekani@gmail.com>
2026-07-18 10:19:31 -0400

    Created test directory and getDirectoryName_test.cxx.

---
+++ test/getDirectoryName_test.cxx
@@ -0,0 +1,64 @@
+#include "babak_lib.h"
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+   char pathname[256];
+   char dirname[256];
+   int exit_st=0;
+   
+   snprintf(pathname, sizeof(pathname), "%s", "/usr/home/file.nii");
+   getDirectoryName(pathname, dirname, sizeof(dirname));
+   printf("pathname = \"%s\"  dirname = \"%s\"", pathname,dirname);
+   if (strcmp(dirname, "/usr/home") == 0) {
+      printf("   (PASSED)\n");
+   } else {
+      printf("   (FAILED)\n");
+      exit_st=1;
+   }
+
+   snprintf(pathname, sizeof(pathname), "%s", "./home/file.nii");
+   getDirectoryName(pathname, dirname, sizeof(dirname));
+   printf("pathname = \"%s\"  dirname = \"%s\"", pathname,dirname);
+   if (strcmp(dirname, "./home") == 0) {
+      printf("   (PASSED)\n");
+   } else {
+      printf("   (FAILED)\n");
+      exit_st=1;
+   }
+
+   snprintf(pathname, sizeof(pathname), "%s", "file.nii");
+   getDirectoryName(pathname, dirname, sizeof(dirname));
+   printf("pathname = \"%s\"  dirname = \"%s\"", pathname,dirname);
+   if (strcmp(dirname, ".") == 0) {
+      printf("   (PASSED)\n");
+   } else {
+      printf("   (FAILED)\n");
+      exit_st=1;
+   }
+
+   snprintf(pathname, sizeof(pathname), "%s", "file.nii");
+   snprintf(pathname, sizeof(pathname), "%s", "/");
+   getDirectoryName(pathname, dirname, sizeof(dirname));
+   printf("pathname = \"%s\"  dirname = \"%s\"", pathname,dirname);
+   if (strcmp(dirname, "/") == 0) {
+      printf("   (PASSED)\n");
+   } else {
+      printf("   (FAILED)\n");
+      exit_st=1;
+   }
+
+   snprintf(pathname, sizeof(pathname), "%s", "/usr/home/");
+   getDirectoryName(pathname, dirname, sizeof(dirname));
+   printf("pathname = \"%s\"  dirname = \"%s\"", pathname,dirname);
+   if (strcmp(dirname, "/usr/home") == 0) {
+      printf("   (PASSED)\n");
+   } else {
+      printf("   (FAILED)\n");
+      exit_st=1;
+   }
+
+   exit(exit_st);
+}
