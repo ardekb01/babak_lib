315caeee858b602959945adf6688070de9796127
Ardekani <babak.ardekani@gmail.com>
2026-07-18 11:50:13 -0400

    Initial commit

---
+++ include/check_nifti_file_extension.h
@@ -0,0 +1,6 @@
+#ifndef CHECK_NIFTI_FILE_EXTENSION_H
+#define CHECK_NIFTI_FILE_EXTENSION_H
+
+int check_nifti_file_extension(const char *filename);
+
+#endif
--- src/checkNiftiFileExtension.cpp
+++ src/checkNiftiFileExtension.cpp
@@ -1,32 +1,34 @@
 #include <string.h>
 
 // Returns 1 if filename has a .hdr or .nii extension, 0 otherwise.
+// Correctly recognizes:
+//    image.hdr
+//    image.nii
+// Correctly rejects:
+//    image.hdr.gz
+//    image.nii.gz
+//    image.HDR
+//    image.NII
 int checkNiftiFileExtension(const char *filename)
 {
-   int L; // number of characters in the filename
+   size_t filenameLength;
+   const char *extension;
 
-   L = strlen(filename);
+   if(filename == nullptr)
+      return 0;
 
-   if(L < 4)
+   filenameLength = strlen(filename);
+
+   if(filenameLength < 4)
+      return 0;
+
+   extension = filename + filenameLength - 4;
+
+   if(strcmp(extension, ".hdr") == 0 ||
+      strcmp(extension, ".nii") == 0)
    {
-      return(0);
+      return 1;
    }
 
-   if(filename[L - 4] == '.' &&
-      filename[L - 3] == 'h' &&
-      filename[L - 2] == 'd' &&
-      filename[L - 1] == 'r')
-   {
-      return(1);
-   }
-
-   if(filename[L - 4] == '.' &&
-      filename[L - 3] == 'n' &&
-      filename[L - 2] == 'i' &&
-      filename[L - 1] == 'i')
-   {
-      return(1);
-   }
-
-   return(0);
+   return 0;
 }
