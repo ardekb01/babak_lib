22a8ae3a2665cf2bc535f6eedfae1f260eb98669
Ardekani <babak.ardekani@gmail.com>
2026-08-06 12:27:29 -0400

    Major algorithmic improvement to findInitialNormalVector() by varying the z component of the centroid to find a better NCC.

--- src/findInitialNormalVector.cpp
+++ src/findInitialNormalVector.cpp
@@ -1,8 +1,9 @@
-#include <math.h>
+#include <cmath>
 #include <cfloat>
 #include "babak_lib.h"
 
 // have to limit the search considering the fact that the input image will be almost PIL
+// The input image must be in PIL orientation.
 bool findInitialNormalVector(short *image, const DIM &dim, float &A, float &B, float &C)
 {
    if(image == nullptr)
@@ -14,16 +15,34 @@ bool findInitialNormalVector(short *image, const DIM &dim, float &A, float &B, f
    if(dim.dx <= 0.0f || dim.dy <= 0.0f || dim.dz <= 0.0f)
       return false;
 
-   float dum;        /* dummy variable */
-   float a, b, c;    /* direction cosines  ax+by+cz=d */
-   float d;          /* the d in ax+by+cz=d */
-   float A1, B1, C1; /* Ax+By+Cz=1 */
+   // compute x_cm,y_cm,z_cm the coordinates of the image "center of gravity"
+   // in (mm) with respect to the FOV center as origin.
+   float x_cm, y_cm, z_cm;
+   if( !intensity_weighted_centroid(image, dim.nx, dim.ny, dim.nz,
+      dim.dx, dim.dy, dim.dz,
+      x_cm, y_cm, z_cm) )
+   {
+      return false;
+   }
+
+   // Ensure that the plane does not pass through the origin.
+   if( std::abs(z_cm) <=  FLT_EPSILON)
+   {
+      z_cm = 0.1f;
+   }
+
+   float dum;
+   float a, b, c;    // direction cosines  ax + by + cz = d 
+   float d;
+
+   // Ax + By + Cz = 1
+   float A1, B1, C1;
+
    float cc;
-   float ccmax;
+   float ccmax = 0.0f;
 
    /* Coordinates of the input image "center of gravity" in mm.
       Origin is taken to be the center of the image volume. */
-   float x_cm, y_cm, z_cm;
 
    double pi;
    double phi0 = 20.0;
@@ -37,13 +56,13 @@ bool findInitialNormalVector(short *image, const DIM &dim, float &A, float &B, f
    double arclength;
    int N;                      // number of samples
 
-   pi = 4.0 * atan(1.0);
+   pi = 4.0 * std::atan(1.0);
 
    // convert from degrees to radians
    phi0 = pi * phi0 / 180.0;
    delphi = pi * delphi / 180.0;
 
-   nrings = (int)ceil(phi0 / delphi) + 1;
+   nrings = static_cast<int>(std::ceil(phi0 / delphi)) + 1;
 
    cumulativelength = (double *)calloc(nrings, sizeof(double));
   
@@ -60,7 +79,7 @@ bool findInitialNormalVector(short *image, const DIM &dim, float &A, float &B, f
       cumulativelength[i] = totallength;
    }
 
-   N = (int)floor(totallength / delphi) + 1;
+   N = static_cast<int>(std::floor(totallength / delphi)) + 1;
 
    theta = (double *)calloc(N, sizeof(double));
    
@@ -99,64 +118,55 @@ bool findInitialNormalVector(short *image, const DIM &dim, float &A, float &B, f
       }
    }
 
-   // compute x_cm,y_cm,z_cm the coordinates of the image "center of gravity"
-   // in (mm) with respect to the image volume center as origin
-   intensity_weighted_centroid(
-      image,
-      dim.nx,
-      dim.ny,
-      dim.nz,
-      dim.dx,
-      dim.dy,
-      dim.dz,
-      x_cm,
-      y_cm,
-      z_cm);
-
    //printf("\n******x_cm=%7.3f y_cm=%7.3f z_cm=%7.3f (mm)\n",x_cm,y_cm,z_cm);
    //printf("i = %f\n", (x_cm + dim.dx*(dim.nx-1.0)/2.0)/1.5 );
    //printf("j = %f\n", (y_cm + dim.dy*(dim.ny-1.0)/2.0)/0.859375 );
    //printf("k = %f\n", (z_cm + dim.dz*(dim.nz-1.0)/2.0)/0.859375 );
 
-   ccmax = 0.0;
+   A1 = 0.0f;
+   B1 = 0.0f;
+   C1 = 1.0f / z_cm;
 
-   for(int i = 0; i < N; i++)
+   for(float z = z_cm - 5.0f; z <= z_cm + 5.0f; z += 1.0f)
    {
-      /* The samples theta and phi define a direction in space. Find the
-         unit vector (a,b,c) in that direction. */
-      a = (float)(sin(phi[i]) * cos(theta[i]));
-      b = (float)(sin(phi[i]) * sin(theta[i]));
-      c = (float)cos(phi[i]);
-
-      d = a * x_cm + b * y_cm + c * z_cm;
-
-      /* make sure d is non-negative */
-      if(d < 0.0)
+      for(int i = 0; i < N; i++)
       {
-         a *= -1.0;
-         b *= -1.0;
-         c *= -1.0;
-         d *= -1.0;
-      }
+         /* The samples theta and phi define a direction in space. Find the
+         unit vector (a,b,c) in that direction. */
+         a = (float)(std::sin(phi[i]) * std::cos(theta[i]));
+         b = (float)(std::sin(phi[i]) * std::sin(theta[i]));
+         c = (float)std::cos(phi[i]);
 
-      /* find the cross-correlation between image and its reflection
+         d = a * x_cm + b * y_cm + c * z;
+
+         /* make sure d is non-negative */
+         if(d < 0.0f)
+         {
+            a *= -1.0f;
+            b *= -1.0f;
+            c *= -1.0f;
+            d *= -1.0f;
+         }
+
+         /* find the cross-correlation between image and its reflection
          about the plane ax+by+cz=d */
 
-      if(d > FLT_EPSILON)
-      {
-         cc = symm_objective_func(image, dim, a / d, b / d, c / d);
-
-         if(cc > ccmax)
+         if(d > FLT_EPSILON)
          {
-            ccmax = cc;
-            A1 = a / d;
-            B1 = b / d;
-            C1 = c / d;
+            cc = symm_objective_func(image, dim, a / d, b / d, c / d);
+
+            if(cc > ccmax)
+            {
+               ccmax = cc;
+               A1 = a / d;
+               B1 = b / d;
+               C1 = c / d;
+            }
          }
       }
    }
 
-   dum = (float)sqrt((double)A1 * A1 + B1 * B1 + C1 * C1);
+   dum = (float)std::sqrt((double)A1 * A1 + B1 * B1 + C1 * C1);
    a = A1 / dum;
    b = B1 / dum;
    c = C1 / dum;
@@ -168,7 +178,7 @@ bool findInitialNormalVector(short *image, const DIM &dim, float &A, float &B, f
 
    cc = optimizeNormalVector(image, dim, A1, B1, C1);
 
-   //dum=(float)sqrt((double)A1*A1 + B1*B1 + C1*C1 );
+   //dum=(float)std::sqrt((double)A1*A1 + B1*B1 + C1*C1 );
    //a=A1/dum; b=B1/dum; c=C1/dum; d=1./dum;
    //printf("\nRefined initial guess:");
    //printf("\nplane of symmetry: (%7.3f,%7.3f,%7.3f).(x,y,z) = %7.3f", a,b,c,d);
